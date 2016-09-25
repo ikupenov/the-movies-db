ba126e6064618c059b6730d93149b7db2f2770f3
Iliyan Kupenov <dmxyyyy@gmail.com>
2016-09-25 19:51:18 +0300

    App url and server base changed

--- movies-db/config/firebase-config.js
+++ movies-db/config/firebase-config.js
@@ -2,11 +2,11 @@ import { firebase } from 'firebase';
 
 const firebaseModule = (function () {
     const config = {
-        apiKey: "AIzaSyDOf5Nu3yzkfUBTpqwLsXhxOZiEdpyv6Mw",
-        authDomain: "movies-db-6fc0d.firebaseapp.com",
-        databaseURL: "https://movies-db-6fc0d.firebaseio.com",
-        storageBucket: "movies-db-6fc0d.appspot.com",
-        messagingSenderId: "863798788488"
+        apiKey: "AIzaSyBbTzQlQZOfzJv-ksXnBCEkuXgWNWQhO9o",
+        authDomain: "themovies-db.firebaseapp.com",
+        databaseURL: "https://themovies-db.firebaseio.com",
+        storageBucket: "",
+        messagingSenderId: "786197584680"
     };
 
     firebase.initializeApp(config);
